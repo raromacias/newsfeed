1467184f8de42bb5fa0ba07461d54b9b67141a33
Raro Macias <ggmacias@gmail.com>
2022-11-22 15:07:30 -0500

    added favorite

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -1,17 +1,48 @@
 import styles from './ArticleCard.module.css';
+import { NavLink , useNavigate} from 'react-router-dom';
+import { useContext } from 'react';
+import axios from 'axios';
+import AuthContext from '../../store/authContext';
 
 
 const ArticleCard = ({newsItem}) => {
   console.log(newsItem)
+
+  const navigate = useNavigate()
+  const {userId, token} = useContext(AuthContext)
 
   const fulldate = new Date(newsItem.publishedAt);
   let date = fulldate.toString().split(" ");
   const hour = parseInt(date[4].substring(0, 2));
   const time = hour > 12 ? true : false;
 
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    let body = {
+      description: newsItem.description,
+          urlToImage: newsItem.urlToImage,
+          url: newsItem.url,
+          publishedAt: newsItem.publishedAt,
+          title: newsItem.title,
+          userId 
+    }
+    axios.post(`http://localhost:4545/favorites/${userId}`, body, {
+        headers: {
+            authorization: token
+        }
+    })
+        .then(() => {
+            navigate('/favorites')
+        })
+        .catch(err => console.log(err))
+}
+
+
 
   return (
     <div className={styles.artcontainer}>
+      <form onSubmit={handleSubmit}>
       <img 
         alt={newsItem.title}
         src={
@@ -39,11 +70,13 @@ const ArticleCard = ({newsItem}) => {
           <div className="description">{newsItem.description}</div>
           <span className="readmore">
             read more at{" "}
-            <a href={newsItem.url} target="__blank" className="source">
-            </a>
+            <NavLink to={newsItem.url} target="__blank" className="source">
+            </NavLink>
           </span>
         </div>
       </div>
+      <button>Add to Favorites</button>
+      </form>
     </div>
   )
 }
