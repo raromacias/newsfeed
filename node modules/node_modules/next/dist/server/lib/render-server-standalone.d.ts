92bea45bb1ed61e3711c22828becd881ee024acf
Raro Macias <ggmacias@gmail.com>
2023-05-22 16:23:45 -0400

    Procfile

--- node modules/node_modules/next/dist/server/lib/render-server-standalone.d.ts
+++
@@ -1,7 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'http';
-export declare const createServerHandler: ({ port, hostname, dir, minimalMode, }: {
-    port: number;
-    hostname: string;
-    dir: string;
-    minimalMode: boolean;
-}) => Promise<(req: IncomingMessage, res: ServerResponse) => Promise<void>>;
