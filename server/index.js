7b40f1423911d2b6661057b42318bcc635f85351
Raro Macias <ggmacias@gmail.com>
2023-06-09 10:18:18 -0400

    added header requirements

--- server/index.js
+++ server/index.js
@@ -8,6 +8,18 @@ const {sequelize} = require('./util/database')
 app.use(express.json())
 app.use(cors())
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.use((req, res, next) => {
+  res.send('Welcome to Express');
+});
+
+
+
 const {User} = require('./models/user')
 const {Article} = require('./models/article')
 
